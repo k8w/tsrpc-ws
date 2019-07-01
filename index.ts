568b69633b310ba67f3996e667eead1698d75db2
k8w <twoeo@qq.com>
2019-07-01 22:22:23 +0800

    publish 0.1.0

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import 'k8w-extend-native';
 import { Server } from './src/server/Server';
 import { Client } from './src/client/Client';
+import { ServiceProto } from './src/proto/ServiceProto';
 
 export { Server as TSRPCServerWS };
 export { Client as TSRPCClientWS };
-
+export { ServiceProto as TSRPCServiceProto };
